67e0d92ef957a261650801cee6270af0657200a6
Fabian Fabro <fabrofabian@gmail.com>
2019-02-24 00:49:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,21 +10,75 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var randNum = Math.floor(Math.random() * 100 + 1);
+  var newItem = {
+    itemName: item,
+    itemPrice: randNum
+  }
+  cart.unshift(newItem);
+  return `${newItem.itemName} has been added to your cart.`;
+
 }
 
 function viewCart() {
-  // write your code here
+  var count;
+
+  if (cart.length === 0){
+    return "Your shopping cart is empty."
+  }
+  var inYourCart = "In your cart, you have ";
+  if(cart.length === 1){
+    inYourCart += `${cart[0].itemName} at $${cart[0].itemPrice}.`
+    return inYourCart;
+  }
+  if(cart.length ===2){
+    count = 0;
+    inYourCart += `${cart[count].itemName} at $${cart[count].itemPrice}, `
+    count +=1;
+    inYourCart += `and ${cart[count].itemName} at $${cart[count].itemPrice}.`
+   return inYourCart;
+  }
+
+  if(cart.length > 2){
+    count = 0;
+    while(count <= cart.length - 1){
+      if (count === cart.length - 1)
+      {
+        inYourCart += `and ${cart[count].itemName} at $${cart[count].itemPrice}.`
+        break;
+
+      }
+    inYourCart += `${cart[count].itemName} at $${cart[count].itemPrice}, `
+    count++;
+    }
+    return inYourCart;
+  }
 }
 
 function total() {
-  // write your code here
+  var fullTotal = 0;
+  for(var count = 0; count < cart.length; count++)
+  {
+    fullTotal += cart[count].itemPrice;
+  }return fullTotal;
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(var count = 0; count < cart.length; count++)
+  {
+    if(item === cart[count].itemName)
+    {
+      cart.splice(count, 1);
+      return cart;
+    }
+  }return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(typeof cardNumber !== "undefined"){
+    cart.splice(0, cart.length);
+  }
+  else{
+    return "Sorry, we don\'t have a credit card on file for you."
+  }
 }
